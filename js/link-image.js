f0b1a6295a35090366aaa2c11101e572cc60a9e9
Văn Thông Lê <thonglevan131@gmail.com>
2020-04-26 00:17:26 +0700

    add link to images of logo, and of documentaries about ccp virus origin

---
+++ js/link-image.js
@@ -0,0 +1,39 @@
+Array.from(document.getElementsByClassName('epochtime')).forEach(child => {
+    child.addEventListener('click', () => {
+        clickSound.play()
+        window.open("https://www.theepochtimes.com/")
+    })
+});
+
+[...document.getElementsByClassName('ntd')].forEach(function(child) {
+    child.addEventListener('click', () => {
+        clickSound.play()
+        window.open("https://www.ntd.com/")
+    })
+});
+[...document.getElementsByClassName('john-hobkins')].forEach(function(child) {
+    child.addEventListener('click', () => {
+        clickSound.play()
+        window.open("https://coronavirus.jhu.edu/map.html")
+    })
+});
+
+// NOTE YOUTUBE
+Array.from(document.getElementsByClassName('china-uncensored-youtube')).forEach(child => {
+    child.addEventListener('click', () => {
+        clickSound.play()
+        window.open("https://www.youtube.com/channel/UCgFP46yVT-GG4o1TgXn-04Q")
+    })
+});
+Array.from(document.getElementsByClassName('america-uncovered-youtube')).forEach(child => {
+    child.addEventListener('click', () => {
+        clickSound.play()
+        window.open("https://www.youtube.com/channel/UC_7vFlErTHxVD-IFNB-BFCg")
+    })
+});
+Array.from(document.getElementsByClassName('ntdtv-youtube')).forEach(child => {
+    child.addEventListener('click', () => {
+        clickSound.play()
+        window.open("https://www.youtube.com/user/NTDTV/featured")
+    })
+});
--- js/modal.js
+++ js/modal.js
@@ -1,5 +1,6 @@
 // Get the modal
-var modal = document.getElementById("modal-container");
+var modal_laptop = document.getElementById("modal-container-laptop");
+var modal_mobile = document.getElementById("modal-container-mobile");
 
 // Get the button that opens the modal
 var btn = document.getElementById("modal-btn");
@@ -10,20 +11,25 @@ var span2 = document.getElementsByClassName("close")[1];
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
-    modal.style.display = "flex";
+    if (screen.width >= 1083) {
+        modal_laptop.style.display = "flex";
+    } else {
+        modal_mobile.style.display = "flex";
+    }
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-    modal.style.display = "none";
+    modal_laptop.style.display = "none";
 }
 span2.onclick = function() {
-    modal.style.display = "none";
+    modal_mobile.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
+    if (event.target == modal_laptop || event.target == modal_mobile) {
+        modal_mobile.style.display = "none";
+        modal_laptop.style.display = "none";
     }
 }
